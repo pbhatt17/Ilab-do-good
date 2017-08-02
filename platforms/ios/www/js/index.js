2e0aff070039c07eb9c4fa7d0eff2e143cd49c97
JckICF <julien.kilian@icf.com>
2017-08-02 16:10:57 -0400

    Cordova build

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -28,6 +28,9 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        console.log(window.open);
+        window.open = cordova.InAppBrowser.open;
+        console.log(window.open);
     },
 
     // Update DOM on a Received Event
