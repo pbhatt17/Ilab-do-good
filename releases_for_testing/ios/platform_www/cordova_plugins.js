ff2a6c8ca526cbd1991a665cc916a26aa317dd63
--get-all <akshaysanganal@gmail.com>
2017-08-29 11:48:50 -0400

    updated for iOS next round of testing

--- releases_for_testing/ios/platform_www/cordova_plugins.js
+++ releases_for_testing/ios/platform_www/cordova_plugins.js
@@ -10,6 +10,14 @@ module.exports = [
         ]
     },
     {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
         "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
         "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
         "pluginId": "cordova-plugin-wkwebview-engine",
@@ -28,6 +36,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-inappbrowser": "1.7.1",
+    "cordova-plugin-statusbar": "2.2.3",
     "cordova-plugin-whitelist": "1.3.2",
     "cordova-plugin-wkwebview-engine": "1.1.3",
     "cordova-plugin-wkwebview-file-xhr": "1.0.0"
